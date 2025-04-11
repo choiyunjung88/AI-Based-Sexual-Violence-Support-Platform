8d627e0cce44df53fdd3ac65cc621b41dff452d2
JH_N <hhhbbs1028@sookmyung.ac.kr>
2025-04-11 17:34:57 +0900

    connect test

---
+++ backend/src/main/java/com/capstone/SafeHug/SafeHugFrontConnectTest.java
@@ -0,0 +1,14 @@
+package com.project.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SafeHugFrontConnectTest {
+
+    @RequestMapping("/test")
+    public String Test(){
+
+        return "connection test";
+    }
+}
