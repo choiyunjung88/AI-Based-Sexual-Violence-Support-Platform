8ba661a5b475a2cc270873aeec20bf059adefc6c
JH_N <hhhbbs1028@sookmyung.ac.kr>
2025-04-11 17:21:13 +0900

    backend init

---
+++ backend/src/main/java/com/capstone/SafeHug/SafeHugApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.SafeHug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeHugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeHugApplication.class, args);
+	}
+
+}
